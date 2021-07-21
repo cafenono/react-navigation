0f7f53688d8af6773298f2b08c46e51404ea2d92
Julien <senxation@gmail.com>
2021-07-21 16:21:14 +0900

    gitpkg

---
+++ lib/typescript/src/utils/CardAnimationContext.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import type { StackCardInterpolationProps } from '../types';
+declare const _default: React.Context<StackCardInterpolationProps>;
+export default _default;
---
+++ lib/typescript/src/utils/PreviousSceneContext.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import type { Scene } from '../types';
+declare const PreviousSceneContext: React.Context<Scene<any>>;
+export default PreviousSceneContext;
